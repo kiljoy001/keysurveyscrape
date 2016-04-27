12397e37690d078fa119de6fa8f333a42a9c26fc
kiljoy001 <kiljoy001@gmail.com>
2016-04-27 13:23:49 -0700

    now able to get to second page & find sub li items

--- ex1.py/execute.py
+++ ex1.py/execute.py
@@ -7,7 +7,8 @@ from selenium.common.exceptions import TimeoutException
 import time
 #login to keysurvey
 #driver = webdriver.PhantomJS(executable_path=r'C:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
-driver = webdriver.Chrome(executable_path=r'C:\phantomjs-2.1.1-windows\bin\chromedriver.exe')
+#driver = webdriver.Chrome(executable_path=r'C:\phantomjs-2.1.1-windows\bin\chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'E:\Projects\chromedriver.exe')
 driver.get('https://app.keysurvey.com/Member/UserAccount/UserLogin.action')
 eleUsername = driver.find_element_by_id("login")
 elePassword = driver.find_element_by_id("password")
@@ -16,21 +17,16 @@ elePassword.send_keys("R3search1")
 driver.find_element_by_id("loginButton").click()
 #https://app.keysurvey.com/app/action/Home/view/main/
 #select reports link
-checkUrl = driver.current_url
+driver.find_element_by_xpath("//a[@href='/Member/ReportWizard/dashboard.do ']").click()
 print(driver.current_url)
-try:
-    driver.execute_script("function login() {document.getElementByName('killConcurrentSessions').setAttribute('value','true');}")
-    print("alert accepted")
-except TimeoutException:
-    print("No alert!")
-print(driver.current_url)
-#driver.find_element_by_xpath("//a[@title='Reports']").click()
 #https://app.keysurvey.com/app/action/report/Home/view/custom/
 #open main link
-#print(driver.page_source)
-driver.find_element_by_id("main").click()
-allLinks = driver.find_elements_by_xpath("//a[@href='#']")
-for items in allLinks:
-    print ("Values are: {0}, {1}, {2}".format(items.get_attribute("class"), items.get_attribute("id"), items.get_attribute("title")))
-
-driver.close()
+element = WebDriverWait(driver, 10).until(
+EC.element_to_be_clickable((By.XPATH, "//*[@id='main']")))
+main = driver.find_element_by_xpath("//*[@id='main']")
+main.click()
+list = driver.find_element_by_class_name("aTreeMenu")
+lilist = list.find_elements_by_tag_name("li")
+print(len(lilist))
+#release browser
+#driver.close()
