7fcf2e889edefd335c39d26d2e3ad62bf45eee41
kiljoy001 <kiljoy001@gmail.com>
2016-04-28 22:15:17 -0700

    trying to get click to open up the tree

--- ex1.py/execute.py
+++ ex1.py/execute.py
@@ -7,8 +7,8 @@ from selenium.common.exceptions import TimeoutException
 import time
 #login to keysurvey
 #driver = webdriver.PhantomJS(executable_path=r'C:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
-#driver = webdriver.Chrome(executable_path=r'C:\phantomjs-2.1.1-windows\bin\chromedriver.exe')
-driver = webdriver.Chrome(executable_path=r'E:\Projects\chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'C:\phantomjs-2.1.1-windows\bin\chromedriver.exe')
+#driver = webdriver.Chrome(executable_path=r'E:\Projects\chromedriver.exe')
 driver.get('https://app.keysurvey.com/Member/UserAccount/UserLogin.action')
 eleUsername = driver.find_element_by_id("login")
 elePassword = driver.find_element_by_id("password")
@@ -25,20 +25,22 @@ element = WebDriverWait(driver, 10).until(
 EC.element_to_be_clickable((By.XPATH, "//*[@id='main']")))
 main = driver.find_element_by_xpath("//*[@id='main']")
 main.click()
-#list = driver.find_element_by_class_name("aTreeMenu")
+
 element = WebDriverWait(driver, 10).until(
 EC.element_to_be_clickable((By.XPATH, "//*[@id='treeContainer']/ul/li[2]/ul//li")))
 list = driver.find_elements_by_xpath("//*[@id='treeContainer']/ul/li[2]/ul//li/a")
-#lilist = list.find_elements_by_xpath("//li[@class='canNotCreate open notActiveNode']")
-#mainlist = lilist.find_elements_by_xpath("//li[@class='canNotCreate open notActiveNode']")
-#ullist = lilist.find_element_by_tag_name("ul")
-#sublilist = ullist.find_elements_by_tag_name("li")
+
 
 print(len(list), )
 for entry in list:
-   #atagitem = entry.find_element_by_tag("class")
     print(entry.get_attribute("title"))
+    entry.click()
+    element = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//*[@id='treeContainer']/ul/li[2]/ul//li//ul//li")))
+    list = driver.find_elements_by_xpath("//*[@id='treeContainer']/ul/li[2]/ul//li//ul//li")
+
    #if entry.get_attribute("class") == "canNotCreate open notActiveNode":
        #entry.find_elements_by_xpath("//li/a/")
 #release browser
 #driver.close()
+
