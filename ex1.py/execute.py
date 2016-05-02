656efd2872dd1cc5f03b0d03b2d69c91f89bf001
Scott Guyton <kiljoy001@gmail.com>
2016-05-02 14:30:23 -0700

    Loops through tree, trying to correlate  make a relationship table to group surveys

--- ex1.py/execute.py
+++ ex1.py/execute.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from itertools import cycle
 from selenium.common.exceptions import TimeoutException
 
 import time
@@ -9,7 +10,7 @@ import time
 #driver = webdriver.PhantomJS(executable_path=r'C:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
 #driver = webdriver.Chrome(executable_path=r'C:\phantomjs-2.1.1-windows\bin\chromedriver.exe')
 driver = webdriver.Chrome(executable_path=r'E:\Projects\chromedriver.exe')
-driver.implicitly_wait(15)
+#driver.implicitly_wait(2)
 driver.get('https://app.keysurvey.com/Member/UserAccount/UserLogin.action')
 eleUsername = driver.find_element_by_id("login")
 elePassword = driver.find_element_by_id("password")
@@ -18,31 +19,39 @@ elePassword.send_keys("R3search1")
 driver.find_element_by_id("loginButton").click()
 #https://app.keysurvey.com/app/action/Home/view/main/
 #select reports link
+driver.maximize_window()
 driver.find_element_by_xpath("//a[@href='/Member/ReportWizard/dashboard.do ']").click()
 print(driver.current_url)
 #https://app.keysurvey.com/app/action/report/Home/view/custom/
 #open main link
-element = WebDriverWait(driver, 10).until(
+element = WebDriverWait(driver, 1).until(
 EC.element_to_be_clickable((By.XPATH, "//*[@id='main']")))
 main = driver.find_element_by_xpath("//*[@id='main']")
 main.click()
 
-element = WebDriverWait(driver, 10).until(
+element = WebDriverWait(driver, 1).until(
 EC.element_to_be_clickable((By.XPATH, "//*[@id='treeContainer']/ul/li[2]/ul//li")))
 list = driver.find_elements_by_xpath("//*[@id='treeContainer']/ul/li[2]/ul//li/a")
 
 print(len(list), )
 for entry in list:
-    #WebDriverWait(driver, 120)
+    element = WebDriverWait(driver, 5).until(
+    EC.element_to_be_clickable((By.XPATH, "//*[@id='treeContainer']/ul/li[2]/ul//li/a")))
     entry.click()
-    #WebDriverWait(driver, 120)
-    newlist = entry.find_elements_by_class_name("surveyFolder")
+    newlist = entry.find_elements_by_xpath("//*[@id='treeContainer']/ul/li[2]/ul//li/ul//li//a")
     print(len(newlist))
-    for items in newlist:
-        if len(newlist) > 0:
-            #WebDriverWait(driver, 360)
-            items.click()
-            #WebDriverWait(driver, 360)
+for items in newlist:
+    if len(newlist) > 0:
+        element = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='treeContainer']/ul/li[2]/ul//li/ul//li//a")))
+        items.click()
+for items in list:
+    print(items.get_attribute("data-rights"), items.get_attribute("title"), items.get_attribute("class"), items.get_attribute("id"))
+    for stuff in cycle(newlist):
+        if not stuff.get_attribute("class") == "last":
+                print(stuff.get_attribute("data-rights"), stuff.get_attribute("title"), stuff.get_attribute("id"))
+        else:
+            break
 
         #print(entry.get_attribute("title"), items.get_attribute("title"))
 #//*[@id="treeContainer"]/ul/li[2]/ul
