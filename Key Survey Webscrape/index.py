646d68930b10190ec6d3fdd69b5833be18be5e82
Scott Guyton <kiljoy001@gmail.com>
2016-11-16 15:08:53 -0800

    additional try/except block added to try to click on the unexpected alert popups boxes, passes if no box shows in five seconds

--- Key Survey Webscrape/index.py
+++ Key Survey Webscrape/index.py
@@ -220,11 +220,22 @@ def inner_loop():
                     EC.element_to_be_clickable((By.LINK_TEXT, "Print to PDF")))
                 pdfClick.click()
                 if check_jquery():
-                    driver.find_element_by_id('printToPDF').click()
-                    # explicit wait that checks if json returns anything other than 'progress' msg,
-                    #  long wait time added to let the file download. Currently set to 5 minutes
-                    WebDriverWait(driver, 300, 5).until(lambda check: pdf_json_check(reports[1], reports[0])
-                       )
+                    try:
+                        driver.find_element_by_id('printToPDF').click()
+                        # explicit wait that checks if json returns anything other than 'progress' msg,
+                        # long wait time added to let the file download. Currently set to 5 minutes
+                        # additional try/except block added to try to click on the unexpected alert popups boxes, passes
+                        # if no box shows in five seconds
+                        try:
+                            WebDriverWait(driver, 5).until(EC.alert_is_present())
+                            driver.switch_to.alert().accept()
+                        except TimeoutException:
+                            pass
+                        except NoAlertPresentException:
+                            pass
+                        WebDriverWait(driver, 300, 5).until(lambda check: pdf_json_check(reports[1], reports[0]))
+                    except UnexpectedAlertPresentException:
+                        driver.switch_to.alert().accept()
                     javaCheck = WebDriverWait(driver, 30, .125).until(
                         EC.element_to_be_clickable((By.XPATH, "//*[@id='emptySel']/a")))
                     if javaCheck:
