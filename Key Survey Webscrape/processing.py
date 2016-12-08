f383da042b9ffd3804bc2ecca4bec49b69ee7224
Scott Guyton <kiljoy001@gmail.com>
2016-12-08 13:16:03 -0800

    added function that gets the amount of unique items per a survey downloaded

--- Key Survey Webscrape/processing.py
+++ Key Survey Webscrape/processing.py
@@ -1,6 +1,6 @@
 import os
-
-
+import re
+from collections import Counter
 def get_list():
     if os.path.isfile('names.txt'):
         with open('names.txt', 'r+') as file:
@@ -13,3 +13,23 @@ def get_list():
             fileout.write('%s\n' % item)
         fileout.close()
 
+
+def get_unique_surveys():
+    if os.path.isfile('filterednames.txt'):
+        # processing file to get the unique survey numbers
+        with open('filterednames.txt', 'r+') as file:
+            text = file.read()
+            surveylist = []
+            surveydictionary = {}
+            for item in text:
+                surveyno = re.search('survey:\d\d\d\d\d\d', item)
+                surveylist.append(surveyno)
+                get_unique = set(surveylist)
+                for each in get_unique:
+                    surveydictionary[each] = Counter(surveylist).values()
+        # write out results with key value pairs representing the survey and how many times it shows up in the list
+            file.close()
+            fileout = open('surveys.txt', 'r+')
+            for k,v in surveydictionary.items():
+                fileout.write('{0} number:{1}\n'.format(k, v))
+            fileout.close()
