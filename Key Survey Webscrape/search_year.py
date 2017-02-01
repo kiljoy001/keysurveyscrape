9fd6729c169d8ad158d64314b35718c33a28aef2
Scott Guyton <kiljoy001@gmail.com>
2017-02-01 15:14:25 -0800

    developed csv.reader to open files

--- Key Survey Webscrape/search_year.py
+++ Key Survey Webscrape/search_year.py
@@ -1,12 +1,51 @@
 import os
 import re
 import csv
-import chardet
+import codecs, chardet
+# from multiprocessing import Pool
+#
+# pool = Pool()
 
-files= os.listdir(r'C:\Users\User\Downloads')
+
+# def process(path):
+#     files = os.listdir(path)
+#     # get half
+#     if len(files) % 2 == 0:
+#         r1 = pool.apply_async(chardet.detect(open(str(path+"{0}".format(files[range(len(files)/2)])), "rb").read()))
+#         r2 = pool.apply_async(chardet.detect(open(str(path + "{0}".format(files[range(0, len(files)/2, len(files)/2)])), "rb")).read())
+#         answer1 = r1.get(timeout=60)
+#         answer2 = r2.get(timeout=60)
+#         if answer1 is not None:
+#             with open("format.txt", "w") as newFile:
+#                 for key, value in answer1.items():
+#                     newFile.write("{0}, {1}\n".format(key, value))
+#                 newFile.close()
+#         if answer2 is not None:
+#             with open("format.txt", "w") as newFile:
+#                 for key, value in answer1.items():
+#                     newFile.write("{0}, {1}\n".format(key, value))
+#                 newFile.close()
+#     else:
+#         get_all = pool.apply_async(chardet.detect(open(str(path + "{0}".format(files[range(len(files))])), "rb").read()))
+#         one_blow = get_all.get(timeout=120)
+#         if one_blow is not None:
+#             with open("format.txt", "w") as newFile:
+#                 for key, value in one_blow.items():
+#                     newFile.write("{0}, {1}\n".format(key, value))
+#                 newFile.close()
+#
+# process("C:\\Users\\User\\Downloads\\")
+files = os.listdir(r'C:\\Users\\User\\Downloads\\')
 for file in files:
     if '.csv' in file:
-        chardet.detect(open(os.path.abspath(r'C:\Users\User\Downloads\{0}'.format(file)), "rb"))
-        # pattern = re.compile()
-        #for i, line in chardet.detect(open(os.path.abspath(r'C:\Users\User\Downloads\{0}'.format(file)), newline='', encoding='cp1252')):
-            # print(line)
+        pattern = re.compile('2013 - 2014')
+        chardetOpen = open(r'C:\Users\User\Downloads\{0}'.format(file), "rb").read()
+        formatDict = chardet.detect(chardetOpen)
+        with open("format.txt", "r+") as newFile:
+            for key, value in formatDict.items():
+                newFile.write("{0}, {1}, {2}\n".format(file, key, value))
+            newFile.close()
+        with codecs.open(os.path.abspath(r'C:\Users\User\Downloads\{0}'.format(file)), "r", "utf8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
